2972aba21cd75fa8cd725a0c5481bf935308ed29
Angelica <angelica.hom@gmail.com>
2015-10-20 06:27:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fhquizone::Application.config.secret_key_base = 'b20a958157358c2d7035834c3e64a1505e778bcbf350000e043c6f6d55f32b76ba111a8758557bdcbc00f78192ad9acd13424b04c9e8498223d475c9fa576c03'
